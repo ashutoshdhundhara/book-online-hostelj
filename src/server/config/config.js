8e5b70c7055ef9580061376aad196ad9915ac5b9
Ashutosh Dhundhara <ashutoshd@wiziq.com>
2016-04-26 18:11:10 +0530

    try-catch with some logging.

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -1,7 +1,7 @@
 var config = {};
 
 config.env = process.env.NODE_ENV || "development";
-config.PORT = 3000;
+config.PORT = 8080;
 config.logsPath = '/tmp/';
 
 module.exports = config;
--- src/server/models/index.js
+++ src/server/models/index.js
@@ -8,22 +8,40 @@ var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
 var logger = require("lib/logger");
-var sequelize = new Sequelize("HostelJ", "root", "", {
-    host: "localhost",
-    dialect: "mysql",
-    dialectOptions: {
-        ssl: false
-    },
-    define: {
-        timestamp: true
-    },
-    freezeTableName: true,
-    pool: {
-        max: 9,
-        min: 0,
-        idle: 10000
-    }
-});
+var config = require("config/config");
+
+try {
+    var sequelize = new Sequelize("HostelJ", "root", "", {
+        host: "localhost",
+        dialect: "mysql",
+        dialectOptions: {
+            ssl: false
+        },
+        define: {
+            timestamp: true
+        },
+        freezeTableName: true,
+        pool: {
+            max: 9,
+            min: 0,
+            idle: 10000
+        },
+        logging: config.env === 'development' ? sequelizeLogger : false
+    });
+} catch (ex) {
+    logger.fatal(ex, "Connection to MySQL failed.");
+    process.exit(1);
+}
+
+/**
+ * Looger for sequelize
+ *
+ * @param string Message
+ * @return void
+ */
+function sequelizeLogger(ex) {
+    logger.info("Inside Sequelize:", ex);
+}
 
 /**
  * Object comprising all `models`, associations, connection
@@ -32,26 +50,36 @@ var sequelize = new Sequelize("HostelJ", "root", "", {
 var db = {};
 
 // Read all models from `models` directory and create a local object
-fs.readdirSync(__dirname)
-    .filter(function (file) {
-        return(
-            file
-            && file.indexOf(".js") !== 0
-            && file != "index.js"
-        );
-    })
-    .forEach(function (file) {
-        var model = sequelize["import"](path.join(__dirname, file));
-        db[model.name] = model;
-    });
+try {
+    fs.readdirSync(__dirname)
+        .filter(function (file) {
+            return(
+                file
+                && file.indexOf(".js") !== 0
+                && file != "index.js"
+            );
+        })
+        .forEach(function (file) {
+            var model = sequelize["import"](path.join(__dirname, file));
+            db[model.name] = model;
+        });
+} catch (ex) {
+    logger.fatal(ex, "Model initialization failed.");
+    process.exit(1);
+}
 
 // Load all `associations`, if any
-Object.keys(db)
-    .forEach(function (model) {
-        if ("associate" in db[model]) {
-            db[model].associate(db);
-        }
-    });
+try {
+    Object.keys(db)
+        .forEach(function (model) {
+            if ("associate" in db[model]) {
+                db[model].associate(db);
+            }
+        });
+} catch (ex) {
+    logger.fatal(ex, "Model associations failed.");
+    process.exit(1);
+}
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
