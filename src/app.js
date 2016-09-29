06d8a78388654b33582b162cb52757fecc4373b2
Ashutosh Dhundhara <ashutoshdhundhara@yahoo.com>
2016-09-29 17:13:14 +0000

    Use ES6

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 var path = require('path');
 var express = require('express');
+var bodyParser = require("body-parser");
+
 var routes = require('config/routes');
 var logger = require("lib/logger");
 
 var app = express();
+
+app.use(bodyParser.json());
 
 // Initialize routes
 routes(app);
---
+++ src/config/index.js
@@ -0,0 +1,13 @@
+var config = {};
+
+config.env = process.env.NODE_ENV || "dev";
+config.PORT = 8080;
+config.logsPath = '/tmp/';
+
+// DB settings
+config.DB_HOST      = process.env.DB_HOST || "localhost";
+config.DB_USER      = process.env.DB_USER || "root";
+config.DB_PASSWORD  = process.env.DB_PASSWORD || "";
+config.DB_NAME      = process.env.DB_NAME || "HostelJ";
+
+module.exports = config;
